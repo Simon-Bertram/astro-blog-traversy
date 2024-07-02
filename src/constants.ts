bb333084e84607e539df9e9e3eeeff5f5ed16b28
Simon-Bertram <simonbertram@hotmail.com>
2024-07-02 08:36:41 +0100

    Separate components

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_DESCRIPTION = 'Articles, Stories and Tutorials for tech people';
+export const SITE_TITLE = 'Site Title';
+export const HOMEPAGE_ARTICLE_LIMIT = 6;
+export const ARTICLES_PER_PAGE = 6;
---
+++ src/utils.ts
@@ -0,0 +1,17 @@
+// Format date to a readable format
+function formatDate(date: Date): string {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  return new Date(date).toLocaleDateString(undefined, options);
+}
+
+// Capitalize the first letter of a string
+function capitalize(str: string): string {
+  if (typeof str !== 'string' || str.length === 0) return str;
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export {formatDate, capitalize}
